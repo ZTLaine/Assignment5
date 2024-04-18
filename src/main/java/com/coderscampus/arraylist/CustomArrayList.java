9415068ffd5a6c2f563aba900421dea888302480
Zack Laine <rumtehk_uh@yahoo.com>
2024-04-18 15:15:45 -0400

    Gettin set up

--- src/main/java/com/coderscampus/arraylist/CustomArrayList.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayList.java
@@ -2,10 +2,19 @@ package com.coderscampus.arraylist;
 
 public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
+	Integer numOfItems = 0;
 
 	@Override
 	public boolean add(T item) {
-		// TODO Auto-generated method stub
+		if(item != null){
+			if(numOfItems != items.length){
+				items[numOfItems] = item;
+				numOfItems++;
+			}
+			else{
+//				num
+			}
+		}
 		return false;
 	}
 
---
+++ src/main/java/com/coderscampus/arraylist/CustomArrayListApp.java
@@ -0,0 +1,12 @@
+package com.coderscampus.arraylist;
+
+public class CustomArrayListApp {
+    public static void main(String[] args) {
+        int[] nums = new int[10];
+        for(int i=0; i<5;i++){
+            nums[i] = i;
+        }
+        System.out.println(nums);
+    }
+
+}
