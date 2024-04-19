de6c2123ce29103ec50b17b2edf68990d5bdaa88
Zack Laine <rumtehk_uh@yahoo.com>
2024-04-19 11:42:02 -0400

    Finished the functions and doing testing

--- src/main/java/com/coderscampus/arraylist/CustomArrayList.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayList.java
@@ -1,3 +1,7 @@
+//  4/19/24
+//  Zack Laine
+//  Assignment 5
+
 package com.coderscampus.arraylist;
 
 public class CustomArrayList<T> implements CustomList<T> {
@@ -7,13 +11,13 @@ public class CustomArrayList<T> implements CustomList<T> {
     @Override
     public boolean add(T item) {
         if (item != null) {
-            if (numOfItems != items.length - 1) {
+            if (numOfItems != items.length) {
                 items[numOfItems] = item;
                 numOfItems++;
             } else {
                 Object[] tempItems = items;
                 items = new Object[numOfItems * 2];
-                for (int i = 0; i <= numOfItems; i++) {
+                for (int i = 0; i <= numOfItems - 1; i++) {
                     items[i] = tempItems[i];
                 }
 
@@ -26,14 +30,25 @@ public class CustomArrayList<T> implements CustomList<T> {
 
     @Override
     public int getSize() {
-        // TODO Auto-generated method stub
-        return 0;
+        return numOfItems;
     }
 
     @Override
     public T get(int index) {
-        // TODO Auto-generated method stub
-        return null;
+        if (index < numOfItems && index >= 0) {
+            return (T) items[index];
+        } else {
+            System.out.println(index + " is not a valid index!");
+            return null;
+        }
+    }
+
+    public void printAll() {
+        for (Object item : items) {
+            if (item != null) {
+                System.out.println(item);
+            }
+        }
     }
 
 }
--- src/main/java/com/coderscampus/arraylist/CustomArrayListApp.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayListApp.java
@@ -1,3 +1,7 @@
+//  4/19/24
+//  Zack Laine
+//  Assignment 5
+
 package com.coderscampus.arraylist;
 
 public class CustomArrayListApp {
@@ -5,11 +9,16 @@ public class CustomArrayListApp {
 //        int[] nums = new int[10];
 //
 //        System.out.println(nums);
-        CustomArrayList<Integer> test = new CustomArrayList<>();
-        for(int i=0; i<11;i++) {
-            test.add(i);
+        CustomArrayList<String> test = new CustomArrayList<>();
+        for (int i = 0; i < 131; i++) {
+            test.add("BAA" + i);
         }
         System.out.println(test.items);
+        test.printAll();
+
+
+        System.out.println("Number of items: " + test.getSize());
+        System.out.println(test.get(71).);
 
     }
 
