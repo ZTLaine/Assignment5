ea2f06d16ae9b974373a2f81eb1bbd19c86ae701
Zack Laine <rumtehk_uh@yahoo.com>
2024-04-19 11:50:04 -0400

    C L E A N

--- src/main/java/com/coderscampus/arraylist/CustomArrayListApp.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayListApp.java
@@ -6,15 +6,6 @@ package com.coderscampus.arraylist;
 
 public class CustomArrayListApp {
     public static void main(String[] args) {
-        CustomArrayList<String> test = new CustomArrayList<>();
-        for (int i = 0; i < 131; i++) {
-            test.add("BAA" + i);
-        }
-        System.out.println(test.items);
-        test.printAll();
-
-        System.out.println("Number of items: " + test.getSize());
-        System.out.println(test.get(71));
     }
 
 }
